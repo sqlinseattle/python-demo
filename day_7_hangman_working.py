9b97ee0f4e7c953da668d33f32993ec91cfa91fd
Andre Ranieri <andre@ranieri.us>
2025-12-18 07:47:28 -0800

    2025-12-18_hangman_day_7

--- day_7_hangman_working.py
+++ day_7_hangman_working.py
@@ -1,35 +1,38 @@
 import sys
 import os
 import random
+from hangman_words import word_list
+from hangman_art import stages, logo
 
 global lives_remaining 
 lives_remaining = 6
 global guessed_letters
 guessed_letters = []
 
+# clear screen at start of game
 def clear_screen():
     # Check the operating system name
     if os.name == 'nt':
         # Command for Windows
         _ = os.system('cls')
     else:
-        # Command for Linux/macOS (posix, osx, linux, etc.)
         _ = os.system('clear')
 
-# TODO-1: - Update the word list to use more complex words than "cat", "dog", and "mouse"
+# TODO-1: - Choose a random word from the dictionary file.
 def choose_word():
-    word_list = ["ardvark", "baboon", "camel"]
     chosen_word = random.choice(word_list)
-    print(chosen_word) # For testing purposes only
+    # print(chosen_word) # For testing purposes only
     return chosen_word
-
 
 # TODO-1 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 def user_input():
     guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print(f"You've already guessed the letter '{guess}'. Try again.")
+        return user_input()  # Prompt the user to guess again
     guessed_letters.append(guess)
     return guess
-
 
 # Create a "display_placeholder" function that prints out the placeholder and updates it when the user guesses a correct letter.
 def display_placeholder(chosen_word, guessed_letters):
@@ -50,33 +53,35 @@ def display_placeholder(chosen_word, guessed_letters):
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 def check_letter(chosen_word, guess):
-    _letter_found = False
+    letter_found = False
     global lives_remaining
     for letter in chosen_word:
             if letter.lower() == guess.lower():
-                print(f"Right {letter}")
-                _letter_found = True
-            else:
-                print(f"Wrong {letter}")
-    if not _letter_found:
+                letter_found = True
+    if not letter_found:
         lives_remaining -= 1
+    return letter_found
 
+ # Break out of choice loop and end game if win or lose condition met
 def end_game(game_won=False):
     if lives_remaining == 0:
-        print("You lose.")
+        print(f"********************You lose ! The word was {chosen_word} ***")
     elif game_won:
-        print("You win.")
+        print("********************You win ! ********************")
     sys.exit()   
 
 def main():
     clear_screen()
-    print("Let's play Hangman!")
+    print(logo)
     chosen_word = choose_word()
     while lives_remaining > 0:
         guess = user_input()
-        check_letter(chosen_word, guess)
+        if check_letter(chosen_word, guess):
+            print(f"Good guess: '{guess}' is in the word.")
+        else:
+            print(f"Sorry: '{guess}' is not in the word.")
         display_placeholder(chosen_word, guessed_letters)
-        print(f"You have {lives_remaining} lives remaining.")
-
+        print(f"******************** You have {lives_remaining}/6 lives remaining. ********************")
+        print(stages[lives_remaining])
 if __name__ == "__main__":
     main()
